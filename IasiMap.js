a0949bab2dff33992b8d211ad587ce9d2e6151f4
Tozlovanu Sandu <77733069+SanduTozlovanu@users.noreply.github.com>
2022-06-04 14:59:33 +0300

    Added Super-user pin template

--- IasiMap.js
+++ IasiMap.js
@@ -1,12 +1,23 @@
-const openPopButtons = document.querySelectorAll('[data-pop-target');
-const closePopButtons = document.querySelectorAll('[data-close-button');
+const openPopButtons = document.querySelectorAll('[data-pop-target]');
+const openPopSuperButtons = document.querySelectorAll('[data-pop-target-super]');
+const closePopButtons = document.querySelectorAll('[data-close-button]');
 const overlay = document.getElementById('overlay');
-console.log('Am intrat');
+const reportButtons = document.querySelectorAll('[report-button]');
+const addReportButtons = document.querySelectorAll('[add-report-button]');
 
 openPopButtons.forEach(button => {
     button.addEventListener('click', () =>{
         const pop = document.querySelector(button.dataset.popTarget)
+        console.log(pop);
         openPop(pop)
+    })
+})
+
+openPopSuperButtons.forEach(button => {
+    button.addEventListener('click', () =>{
+        const pop = document.querySelector(button.dataset.popTargetSuper)
+        console.log(pop);
+        openPopSuper(pop)
     })
 })
 
@@ -29,6 +40,29 @@ function openPop(pop){
     if(pop == null) return
     pop.classList.add('activated');
     overlay.classList.add('activated');
+    for( i = 0 ; i < reportButtons.length;i++)
+    {
+        reportButtons[i].classList.add('disabled');
+    }
+    for( i = 0; i< addReportButtons.length;i++)
+    {
+        addReportButtons[i].classList.remove('disabled');
+    }
+}
+
+function openPopSuper(pop){
+    if(pop == null) return
+    pop.classList.add('activated');
+    overlay.classList.add('activated');
+    for( i = 0 ; i < reportButtons.length;i++)
+    {
+        reportButtons[i].classList.remove('disabled');
+    }
+    for( i = 0; i< addReportButtons.length;i++)
+    {
+        addReportButtons[i].classList.add('disabled');
+    }
+
 }
 
 function closePop(pop){
