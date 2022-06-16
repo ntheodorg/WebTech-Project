90b97088381736c8cce307a718269e87879a8f3e
Nae Theodor-Gabriel <73588948+ntheodorg@users.noreply.github.com>
2022-06-16 11:22:32 +0300

    Fixed login bug

--- routers/commonRouter.js
+++ routers/commonRouter.js
@@ -4,6 +4,7 @@ const { commonRoutes } = require("../settings/_serverSettings");
 
 const commonRouter = new Router();
 commonRouter.post(commonRoutes.signup.route, authController.signup);
+commonRouter.post(commonRoutes.login.route, authController.login);
 commonRouter.post(commonRoutes.signup_superuser.route, authController.signup_SuperUser)
 commonRouter.get(commonRoutes.accountType_get.route, authController.getUserData);
 commonRouter.get(commonRoutes.logout.route, authController.logout);
